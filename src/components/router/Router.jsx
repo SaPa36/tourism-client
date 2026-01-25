66518979720a2c334d9eccaf3ad16550cf43979e
SaPa36 <nursapa2001@gmail.com>
2026-01-25 17:10:29 +0600

    connect router to the main.jsx file

---
+++ src/components/router/Router.jsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter
+  } from "react-router-dom";
+import Root from "../layouts/Root";
+import Home from "../pages/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+            // Define child routes here
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
+    }
+]);
+
+export default router;
