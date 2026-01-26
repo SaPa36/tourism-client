a8ad29f0c156e3d594858d03a5378fe791ec0a63
SaPa36 <nursapa2001@gmail.com>
2026-01-26 10:29:21 +0600

    design Login and Register Page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-
+import loginImg from '../../assets/login.jpg';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <img src="" alt="" />
+        <div className="hero bg-base-200 ">
+            
+            <div className="hero-content mt-7  flex-col lg:flex-row">
+                
+                <div className="text-center shadow-blue-900 shadow-2xl w-1/2 lg:text-left">
+                    
+                    <img className='w-full rounded-2xl' src={loginImg} alt="Login" />
                 </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card bg-white pt-8 pb-8 pl-5 pr-5 w-full max-w-sm shrink-0 shadow-blue-900 shadow-2xl">
+                    <h1 className="text-5xl text-black font-bold text-center">Login now!</h1>
                     <div className="card-body">
                         <fieldset className="fieldset">
-                            <label className="label">Email</label>
+                            <label className="label text-black">Email</label>
                             <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
+                            <label className="label text-black">Password</label>
                             <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <div><a className="link link-hover text-black">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Login</button>
+                            <p className='text-black mt-4'>Are You New Here? Please 
+                                 <Link className='text-blue-500 font-bold' to="/register">  Register</Link></p>
                         </fieldset>
                     </div>
                 </div>
             </div>
+            
         </div>
     );
 };
--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import loginImg from '../../assets/login.jpg';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
+        <div className="hero bg-base-200 ">
+            
+            <div className="hero-content mt-7  flex-col lg:flex-row">
+                
+                <div className="text-center shadow-blue-900 shadow-2xl w-1/2 lg:text-left">
+                    
+                    <img className='w-full rounded-2xl' src={loginImg} alt="Login" />
+                </div>
+                <div className="card bg-white pt-3 pb-3 pl-5 pr-5 w-full max-w-sm shrink-0 shadow-blue-900 shadow-2xl">
+                    <h1 className="text-5xl text-black font-bold text-center">Register now!</h1>
+                    <div className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="label text-black pl-2 font-bold">Name</label>
+                            <input type="text" className="input rounded-xl" placeholder="Name" />
+                            <label className="label text-black pl-2 font-bold">Email</label>
+                            <input type="email" className="input rounded-xl" placeholder="Email" />
+                            <label className="label text-black pl-2 font-bold">Password</label>
+                            <input type="password" className="input rounded-xl" placeholder="Password" />
+                            
+                            <button className="btn btn-neutral rounded-xl mt-4">Register</button>
+                            <p className='text-black mt-4 pl-2 '>Already Have an Account? Please 
+                                 <Link className='text-blue-800 font-bold' to="/login">  Login</Link></p>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
