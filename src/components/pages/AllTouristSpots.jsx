a75dba83be8e0dfe1ea8e6fcef2b44906e656f72
SaPa36 <nursapa2001@gmail.com>
2026-01-28 09:28:59 +0600

    fixed all touris spot part

--- src/components/pages/AllTouristSpots.jsx
+++ src/components/pages/AllTouristSpots.jsx
@@ -69,29 +69,7 @@ const AllTouristSpots = () => {
                 </div>
             
         
-                <div className="text-center mb-12">
-                    <h2 className="text-4xl lg:text-6xl font-black text-white mb-4">
-                        Explore <span className="text-cyan-400">Tourist</span> <span className="text-fuchsia-500">Spots</span>
-                    </h2>
-                    <p className="text-gray-400">Discover breathtaking destinations curated just for you.</p>
-                </div>
-
-                {/* Grid Layout for Cards */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {
-                        touristSpots.map(spot =>
-                            <AllTouristSpot 
-                            key={spot?._id || Math.random()}
-                                spot={spot}
-                                touristSpots={touristSpots}
-                                setTouristSpots={setTouristSpots}
-                                >
-                                
-                            </AllTouristSpot>
-                        )
-                    }
-
-                </div>
+                
             </div>
 
             
